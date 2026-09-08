6557658d46f483eba389104105ffde80343261bf
rakibul875 <rakibulislamashik78@gmail.com>
2026-09-08 18:59:26 +0600

    add FoodCard component to display menu items with add-to-cart functionality

--- src/components/orders/FoodCard.tsx
+++ src/components/orders/FoodCard.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
 import { FoodItem } from "@/app/orders/page";
 import { FiShoppingCart } from "react-icons/fi";
 import Link from "next/link";
-import { redirect } from "next/dist/server/api-utils";
-import { useRouter } from "next/navigation";
+
 
 interface FoodCardProps {
   item: FoodItem;
@@ -22,15 +20,7 @@ interface FoodCardProps {
 const FoodCard: React.FC<FoodCardProps> = ({ item,user }) => {
 
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     router.push("/login");
-  //   }
-  // }, [user, router]);
 
-  // if (!user) {
-  //   return null;
-  // }
  
   return (
     <div className="bg-white rounded-2xl border border-gray-100 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 flex flex-col overflow-hidden group">
